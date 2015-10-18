78a462e2b0aa6f5bdf2c53f907b6a7a55b0e70c1
Roman Proskin <proskin@phystech.edu>
2015-10-18 23:51:06 +0300

    correcter mistake with parsing arguments. Trying to add estimation of iterations number

--- linear_solver/matrix_gen.cpp
+++ linear_solver/matrix_gen.cpp
@@ -1,66 +1,151 @@
 #include <iostream>
 #include <fstream>
+#include <cstdlib>
 #include <ctime>
-#include <stdio.h>
-#include <stdlib.h>
+#include <Eigen/Dense>
+#include <Eigen/LU>
+#include <Eigen/Sparse>
+#include <Eigen/Eigenvalues>
 
+using namespace Eigen;
 using namespace std;
 
-int main()
+int main(int argc, char** argv)
 {
-    srand(time(NULL));
-    ofstream output;
-    output.open("in.txt");
-    int n = 0;
-    cout << "Please, enter N- the order of the matrix\n";
-    cin >> n ;
-    int **a = new int* [n];
-    for (int i = 0; i < n; i++)
-    {
-        a[i] = new int [n];
-    }
+  clock_t begin = clock();
 
-    for (int i = 0; i < n; i++)
+  ifstream input;
+  ofstream output("output.txt");
+  double param = 0.1; // parameter in iterative method
+  if (argc == 1)
+  {
+    cout << "Please provide input file. Usage: <program> <filename>" << endl;
+    cout << "Optional: -p <parameter value>" << endl;
+    exit(-2);
+  }
+  if (argc > 1)
+  {
+    if (strcmp(argv[1], "-p"))
     {
-        for (int j = 0; j < n; j++)
-        {
-            a[i][j] = rand() % 10;
-            //cout << a[i][j] << " ";
-        }
-        //cout << endl;
+      try
+      {
+        input.open(argv[1]);
+      }
+      catch(...)
+      {
+        cout << "## ERROR: Incorrect input filename" << endl;
+        exit(-1);
+      }
+      if (!strcmp(argv[2], "-p")) 
+      {
+        param = strtod(argv[3], NULL);
+      }
     }
-     //printf ("!!!!!!!!!!!!!!!!!!!!\n") ;
-    int* b = new int [n];
-    for (int i = 0; i < n; i++)
+    else
     {
-        b[i] = rand()%10;
-        //cout << b[i] << " ";
+      cout << "Please provide input file. Usage: <program> <filename>" << endl;
+      cout << "Optional: -p <parameter value>" << endl;
+      exit(-1);
     }
-    //cout << endl;
+  }
 
+  double epsilon = 0;
+  int matrix_size = 0;
 
-    output << 0.001 << endl;
-    output << n << endl;
-    for (int i=0; i<n;i++)
-    {
-        output << b[i] << endl;
-    }
+  // get precision and matrix size
+  input >> epsilon;
+  input >> matrix_size;
+  //cout << epsilon << endl;
+  //cout << matrix_size << endl;
 
-    for (int i = 0; i < n; i++)
-    {
-        for (int j = 0; j < n; j++)
-        {
-            output << a[i][j] << endl;
-        }
-    }
+  // declare all necessary matrices & vectors
+  MatrixXd A_Matrix(matrix_size, matrix_size);
+  MatrixXd U_Matrix(matrix_size, matrix_size);
+  MatrixXd L_Matrix(matrix_size, matrix_size);
+  MatrixXd D_Matrix(matrix_size, matrix_size);
+  MatrixXd M_Matrix(matrix_size, matrix_size);
+  MatrixXd F_Matrix(matrix_size, matrix_size);
+  //MatrixXd G_Matrix(matrix_size, matrix_size);
+  VectorXd Value_vector(matrix_size);
+  VectorXd Init_vector(matrix_size);
+  VectorXd Result_vector(matrix_size);
+  //VectorXd g_vector(matrix_size);
+  // set zero approximation to zero vector
+  Init_vector.setZero();
 
-    for (int i = 0; i < n; i++)
+  // input free values
+  for (int i = 0; i < matrix_size; i++)
+  {
+    if (input.eof())
     {
-        free(a[i]);
+      cout << "## ERROR: Not enough data for vector. please provide " << matrix_size << " values!" << endl;
+      input.close();
+      output.close();
+      exit(-1);
     }
-    free(b);
-    free(a);
-    cout << "Matrix is generated\n";
-	output.close();
-    return 0;
+    input >> Value_vector(i);
+  }
+  // input Matrix_A from file
+  for (int i = 0; i < matrix_size; i++)
+  {
+    for (int j = 0; j < matrix_size; j++)
+    {
+      if (input.eof())
+      {
+        cout << "## ERROR: Not enough data for matrix. please provide " << matrix_size * matrix_size << " values!" << endl;
+        input.close();
+        output.close();
+        exit(-1);
+      }
+      input >> A_Matrix(i, j);
+    }
+  }
+
+  // we need symmetrical matrix, so we make it that way
+  Value_vector = A_Matrix.transpose() * Value_vector;
+  A_Matrix = A_Matrix * A_Matrix.transpose();
+  // calculating matrices derived from A
+  U_Matrix = A_Matrix.triangularView<StrictlyUpper>();
+  L_Matrix = A_Matrix.triangularView<StrictlyLower>();
+  D_Matrix = A_Matrix.diagonal().asDiagonal();
+  M_Matrix = L_Matrix + (D_Matrix / (1 + param));
+  F_Matrix = U_Matrix + (param / (1 + param)) * D_Matrix;
+  M_Matrix = M_Matrix.inverse();
+  //G_Matrix = - M_Matrix * F_Matrix;
+  //g_vector = M_Matrix * Value_vector;
+
+  /*
+  // estimating number of iterations
+  SelfAdjointEigenSolver<MatrixXd> es(G_Matrix);
+  double q = es.eigenvalues().maxCoeff();
+  cout << q << endl;
+  int estimate = 0;
+  estimate = ceil(log((epsilon * (1-q))/g_vector.norm()) / log(q));
+  cout << "Estimated number of iterations: " << estimate << endl;
+  */
+
+  // calculating result using iterative method
+  cout << "Parameter: p = " << param << endl; 
+  cout << "Calculating..." << endl;
+  long long int N = 0;
+  for (N = 0; ; N++)
+  {
+    Result_vector = M_Matrix * (Value_vector - F_Matrix * Init_vector);
+    if ((A_Matrix * Result_vector - Value_vector).norm() <= epsilon) break;
+    Init_vector = Result_vector;
+  }
+  // saving the solution
+  output << Result_vector;
+
+  output.close();
+  input.close();
+  clock_t end = clock();
+  double elapsed_secs = double(end - begin) / CLOCKS_PER_SEC;
+  cout << "The method has taken: " << elapsed_secs << " seconds" << endl;
+  cout << "There was " << N + 1 << " iterations" << endl; 
+
+  //cout << A_Matrix * Result_vector << endl << endl;
+  //cout << Value_vector << endl;
+
+  return 0;
 }
